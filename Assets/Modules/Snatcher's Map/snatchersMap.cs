a331b7c4af6d0ff3ab9617a6c5033a37c476b62d
llPlayll <129607915+llPlayll@users.noreply.github.com>
2023-11-18 23:00:47 +0500

    Module Maneuvers.

---
+++ Assets/Modules/Snatcher's Map/snatchersMap.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+
+public class snatchersMap : MonoBehaviour
+{
+
+    [SerializeField] private KMBombInfo Bomb;
+    [SerializeField] private KMAudio Audio;
+
+    static int ModuleIdCounter = 1;
+    int ModuleId;
+    private bool ModuleSolved;
+
+    Dictionary<string, List<string>> deathWishes;
+
+    void Awake()
+    {
+        ModuleId = ModuleIdCounter++;
+        /*
+        foreach (KMSelectable object in keypad) {
+            object.OnInteract += delegate () { keypadPress(object); return false; };
+            }
+        */
+
+        //button.OnInteract += delegate () { buttonPress(); return false; };
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Log(string arg)
+    {
+        Debug.Log($"[Snatcher's Map #{ModuleId}] {arg}");
+    }
+
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+#pragma warning restore 414
+
+    IEnumerator ProcessTwitchCommand(string Command)
+    {
+        yield return null;
+    }
+
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        yield return null;
+    }
+}
